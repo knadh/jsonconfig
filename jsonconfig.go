bf873d65b1cea8237530fddb245e04f53fda8bbc
Kailash Nadh <kailash.nadh@gmail.com>
2015-02-21 11:43:57 +0530

    Idiomatic comments

--- jsonconfig.go
+++ jsonconfig.go
@@ -7,39 +7,39 @@
 package jsonconfig
 
 import (
-	"regexp"
 	"encoding/json"
-	"io/ioutil"
 	"errors"
+	"io/ioutil"
+	"regexp"
 )
 
-func Load(filename string, config interface{})  error {
-	// read file
+func Load(filename string, config interface{}) error {
+	// Read the config file.
 	data, err := ioutil.ReadFile(filename)
-	if(err != nil) {
+	if err != nil {
 		return errors.New("Error reading file")
 	}
 
-	// regex monstrosity because of the lack of lookbehinds/aheads
-	// stand alone comments
+	// Regex monstrosity because of the lack of lookbehinds/aheads.
+
+	// Standalone comments.
 	r1, _ := regexp.Compile(`(?m)^(\s+)?//(.*)$`)
 
-	// numbers and boolean
+	// Numbers and boolean.
 	r2, _ := regexp.Compile(`(?m)"(.+?)":(\s+)?([0-9\.\-]+|true|false|null)(\s+)?,(\s+)?//(.*)$`)
 
-	// strings
+	// Strings.
 	r3, _ := regexp.Compile(`(?m)"(.+?)":(\s+)?"(.+?)"(\s+)?,(\s+)?//(.*)$`)
 
-	// arrays and objects
+	// Arrays and objects.
 	r4, _ := regexp.Compile(`(?m)"(.+?)":(\s+)?([\{\[])(.+?)([\}\]])(\s+)?,(\s+)?//(.*)$`)
-	
 
 	res := r1.ReplaceAllString(string(data), "")
 	res = r2.ReplaceAllString(res, `"$1": $3,`)
 	res = r3.ReplaceAllString(res, `"$1": "$3",`)
 	res = r4.ReplaceAllString(res, `"$1": $3$4$5,`)
 
-	// decode json
+	// Decode json.
 	if err := json.Unmarshal([]byte(res), &config); err != nil {
 		return err
 	}
